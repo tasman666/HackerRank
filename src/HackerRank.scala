465a329b2e0c89167d448761fdd013b70d3cc11b
Arkadiusz Trzepacz <arkadiusz.trzepacz@applica.ai>
2020-09-04 23:59:37 +0200

    hacker rank very big array sum

--- src/HackerRank.scala
+++ src/HackerRank.scala
@@ -5,6 +5,7 @@ object HackerRank {
 
     println(Triplets.compareTriplets(Array(87, 54, 23), Array(67, 68, 21)).mkString(", "))
     println(Sum.simpleArraySum(Array(87, 54, 23)))
+    println(Sum.aVeryBigSum(Array(87, 54, 23)))
 
 
   }
--- src/Sum.scala
+++ src/Sum.scala
@@ -7,4 +7,11 @@ object Sum {
     if (ar.length > 0) ar.sum else throw new IllegalArgumentException("Array shouldn't be empty")
   }
 
+  def aVeryBigSum(ar: Array[Long]): Long = {
+    val valid = ar.forall(item => item >= 0 && item <= Math.pow(10,10))
+    if (!valid) throw new IllegalArgumentException("Item value invalid")
+
+    if (ar.length > 0 && ar.length <= 10) ar.sum else throw new IllegalArgumentException("Array size invalid")
+  }
+
 }
