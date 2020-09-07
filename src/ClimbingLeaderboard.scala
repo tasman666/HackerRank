df3070f6cac27905f1905a009b67a4e73e3bf0d3
Arkadiusz Trzepacz <arkadiusz.trzepacz@applica.ai>
2020-09-07 22:20:52 +0200

    climbing leaderboard - binary search

--- src/ClimbingLeaderboard.scala
+++ src/ClimbingLeaderboard.scala
@@ -1,4 +1,5 @@
 import java.io.PrintWriter
+import java.util
 
 object ClimbingLeaderboard {
 
@@ -8,49 +9,20 @@ object ClimbingLeaderboard {
         throw new IllegalArgumentException("Wrong input")
       }
 
-      climbingLeaderboard(scores, alice, Array())
+      val scoresDist = scores.reverse.distinct
+      alice.map(aliceValue => {
+        val binarySearchResult = util.Arrays.binarySearch(scoresDist, aliceValue)
+        if (binarySearchResult < 0) {
+          (scoresDist.length + 1) -  (Math.abs(binarySearchResult) - 1)
+        } else {
+          scoresDist.length - binarySearchResult
+        }
+      })
+
     }
 
     private def invalidArray(array: Array[Int]) = {
       array.length < 1 || array.length > 2 * Math.pow(10, 5)
-    }
-
-    @scala.annotation.tailrec
-    private def climbingLeaderboard(scores: Array[Int], alice: Array[Int], result: Array[Int]): Array[Int] = {
-      alice.lastOption match {
-        case Some(lastAliceValue) =>
-          val positionResult = positionWithIndex(lastAliceValue, result.headOption.getOrElse(1), 0, scores)
-          println("index " + positionResult)
-          climbingLeaderboard(scores.drop(positionResult._2), alice.dropRight(1), positionResult._1 +: result)
-        case None => result
-      }
-    }
-
-    @scala.annotation.tailrec
-    private def positionWithIndex(aliceValue: Int, positionNr: Int, index: Int, scores: Array[Int]): (Int, Int) = {
-
-      if (invalidValue(aliceValue)) throw new IllegalArgumentException("Wrong input")
-
-      scores match {
-        case Array(firstScore, _*) =>
-          if (invalidValue(firstScore)) throw new IllegalArgumentException("Wrong input")
-          if (aliceValue >= firstScore) {
-            (positionNr, index)
-          } else {
-            scores.tail match {
-              case Array(nextValue, _*) =>
-                positionWithIndex(aliceValue, if (nextValue == firstScore) positionNr else positionNr + 1, index + 1, scores.tail)
-              case _  => (positionNr + 1, index)
-            }
-          }
-        case _ => (positionNr, index)
-      }
-
-
-    }
-
-    private def invalidValue(value: Int) = {
-      value < 0 || value > Math.pow(10, 9)
     }
 
   def main(args: Array[String]) {
