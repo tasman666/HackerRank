564c8bcf09294ea7604bc81f2593e0f86e068078
Arkadiusz Trzepacz <arkadiusz.trzepacz@applica.ai>
2020-09-09 23:21:39 +0200

    ice cream parlor - efficient solution

--- src/IceCreamParlor.scala
+++ src/IceCreamParlor.scala
@@ -1,29 +1,23 @@
+
 object IceCreamParlor {
 
   // Complete the whatFlavors function below.
   def whatFlavors(cost: Array[Int], money: Int) {
-    val result = whatFlavors(cost, 0, money, None)
-    println(result._1 + " " + result._2)
-  }
-
-  @scala.annotation.tailrec
-  private def whatFlavors(cost: Array[Int], dropNr: Int, money: Int, result: Option[(Int, Int)]): (Int, Int) = {
-    result match {
-      case None =>
-        val resultArray = for (
-          (value1, index1) <- cost.drop(dropNr).zipWithIndex;
-          (value2, index2) <- cost.drop(dropNr + 1).zipWithIndex
-          if value1 + value2 == money
-        ) yield {
-          (index1 + 1 + dropNr, index2 + 1 + dropNr + 1)
+    val mapA = scala.collection.mutable.HashMap[Int, Int]()
+    for(i <- cost.indices) {
+        if (mapA.contains(money - cost(i))) {
+          println((mapA(money - cost(i)) + 1) + " " + (i + 1))
+          return
+        } else {
+          mapA += (cost(i) -> i)
         }
-        whatFlavors(cost, dropNr + 1, money, resultArray.headOption)
-      case Some((value1, value2)) => (value1, value2)
     }
   }
 
   def main(args: Array[String]): Unit = {
     whatFlavors(Array(1,4,6,5,3),11)
+    whatFlavors(Array(1,4,6,5,3),5)
+    whatFlavors(Array(1,4,6,5,3),9)
   }
 
 }
