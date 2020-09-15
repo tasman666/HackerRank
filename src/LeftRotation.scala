6e6aa3760bf192dcd2b3521dbd18a4260ed92531
Arkadiusz Trzepacz <a.trzepacz@gmail.com>
2020-09-15 21:55:22 +0200

    left rotation

---
+++ src/LeftRotation.scala
@@ -0,0 +1,28 @@
+import IceCreamParlor.whatFlavors
+
+object LeftRotation {
+
+  // Complete the rotLeft function below.
+  def rotLeft(a: Array[Int], d: Int): Array[Int] = {
+
+    a.zipWithIndex.map( indexWithValue => {
+      val index = indexWithValue._2
+      a((index + d) % a.length)
+    })
+
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    println(rotLeft(Array(1,4,6,5,3),1).mkString(","))
+    println(rotLeft(Array(1,4,6,5,3),2).mkString(","))
+    println(rotLeft(Array(1,4,6,5,3),4).mkString(","))
+  }
+}
+
+// 14653 -> 46531
+//0 -> 4
+//1 -> 0
+//2 -> 1
+//3 -> 2
+//4 -> 3
